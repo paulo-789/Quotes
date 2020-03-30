55a038d6ddf4e67fea4de4dc9eb00466e9477e30
paulo-789 <jorhnepaulone@gmail.com>
2020-03-30 21:36:09 +0300

    imported quotes

--- src/app/quotes-detail/quotes-detail.component.ts
+++ src/app/quotes-detail/quotes-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import {Quotes} from '../quotes';
 
 @Component({
   selector: 'app-quotes-detail',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesDetailComponent implements OnInit {
 
+  @input() quotes:Quotes;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
   }
 
 }
