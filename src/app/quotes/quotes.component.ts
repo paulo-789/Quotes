b7ffa930197d37f0f571cdd8d42678f9dc674b63
paulo-789 <jorhnepaulone@gmail.com>
2020-03-31 20:31:29 +0300

    styled the heading

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,10 +9,10 @@ import {Quotes} from '../quotes';
 export class QuotesComponent implements OnInit {
 
   quotes: Quotes[]=[
-    new Quotes(1,'Paul','author-Why The Lucky Stiff','"when you dont create things, you become defined by your tastes rather than ability. your tastes only narrow & exclude people. so create."'),
-    new Quotes(2,'Rael','author__ Harold Abelson, Structure and Interpretation of Computer Programs','"Programs must be written for people to read, and only incidentally for machines to execute"'),
-    new Quotes(3,'Jane','author___Rick Cook, The Wizardry Compiled','"Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots. So far, the Universe is winning."'),
-    new Quotes(4,'Kennedy','author___ Martin Fowler','“Any fool can write code that a computer can understand. Good programmers write code that humans can understand.”')
+    new Quotes(1,'Paul','Lucky Stiff','"when you dont create things, you become defined by your tastes rather than ability. your tastes only narrow & exclude people. so create."'),
+    new Quotes(2,'Rael',' Harold Abelson','"Programs must be written for people to read, and only incidentally for machines to execute"'),
+    new Quotes(3,'Jane','Rick Cook,','"Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots. So far, the Universe is winning."'),
+    new Quotes(4,'Kennedy','Martin Fowler','“Any fool can write code that a computer can understand. Good programmers write code that humans can understand.”')
   ];
   completequotes(isComplete,index){
     if(isComplete){
